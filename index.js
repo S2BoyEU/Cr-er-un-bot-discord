0ba60a6ab45538605b5b8a189478af8e14286d51
Kiban <57617307+SHADOWkebab@users.noreply.github.com>
2020-07-25 17:15:06 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 const config = require("./config.json")
+const prefix = require("./config.json")
+
+const fs = require("fs")
 
 bot.on("ready", async message => {
-console.log(`${bot.user.username} est allumé !`)
-bot.user.setActivity("votre statut pérsonnalisée", {type: 'WATCHING'})
+    console.log(`${bot.user.username} est allumé !`)
+    bot.user.setActivity("Je suis le bot", {type: 'WATCHING'})
+})
+
+fs.readdir("./Commandes/", (err, files) => {
+    if(err)console.log(err)
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Aucune commande trouvée")
+        return
+    }
+    jsfile.forEach((f,i) =>{
+        let proprs = require(`./Commandes/${f}`)
+        console.log(`${f} Chargée ) 100%`)
+        bot.commands.set(props.help.name.props)
+    })
+    fs.readdir("./events/",(error, f) => {
+        if(error)console.log(error)
+        console.log(`${f.length} event en chargement !`)
+        f.forEach((f) => {
+            const events = require(`./events/${f}`)
+            const event = f.split(".")[0]
+            bot.on(event, events.bind(null, client))
+        })
+    })
 })
 
 bot.login(config.token)
-
-
-
-créer un fichier config.json:
-{
-"token ": " Votre token "
-}
-
