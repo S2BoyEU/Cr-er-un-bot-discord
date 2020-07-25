19fbe9245a70f71c9fedabc16649c9f5dac723a2
Kiban <57617307+SHADOWkebab@users.noreply.github.com>
2020-07-25 15:26:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,16 +3,8 @@ const bot = new Discord.Client()
 const config = require("./config.json")
 
 bot.on("ready", async message => {
-console.log(`${bot.user.username} est allumé !`)
-bot.user.setActivity("votre statut pérsonnalisée", {type: 'WATCHING'})
+    console.log(`${bot.user.username} est allumé !`)
+    bot.user.setActivity("Votre statut", {type: 'WATCHING'})
 })
 
 bot.login(config.token)
-
-
-
-créer un fichier config.json:
-{
-"token ": " Votre token "
-}
-
